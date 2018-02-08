8fda036e991490f482c646069332c0f17621166f
Sujan Abraham <itssujan@gmail.com>
2018-02-08 11:24:38 -0500

    express server

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const request = require("request");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+let cache = require("memory-cache");
+
+app.get("/", (req, res) => res.send("Welcome to busbud!"));
+
+let busbudAPIOpts = {
+  url: "https://napi.busbud.com/x-departures/dr5reg/f25dvk/2018-07-07",
+  headers: {
+    Accept:
+      "application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/",
+    "X-Busbud-Token": process.env.BUSBUD_TOKEN
+  }
+};
+
+app.get("/departures", (req, res) => {
+  request(busbudAPIOpts, callback);
+  res.status(200);
+  res.send("");
+});
+
+app.get("/getdepartures", (req, res) => {
+  let departures = cache.get("departures");
+  console.log(departures.complete);
+  // while (!departures.complete) {
+  //   console.log("pooling");
+  // }
+  res.send({ data: departures });
+});
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    cache.put("departures", info);
+    console.log(info.complete);
+  } else {
+    console.log("ERROR");
+  }
+}
+
+app.listen(port, () => console.log("server running on port " + port));
